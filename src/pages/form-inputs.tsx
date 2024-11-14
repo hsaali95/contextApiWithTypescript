8dbd1838b0d97a186b26fe5f0d8a96edd3a93ea8
Ahmed Ali <ahmed.ali@softvira.com>
2024-11-14 14:22:55 +0500

    input fields with from and with onSubmit and type submit

---
+++ src/pages/form-inputs.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { IForm } from "../types";
+
+const FormInputs = () => {
+  const [formData, setFormData] = useState<IForm>({
+    myname: "",
+    phone: "",
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <br />
+        <label>Name</label>
+        <input
+          type="text"
+          name={"myname"}
+          value={formData.myname}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>Phone</label>
+        <input
+          type={"number"}
+          name={"phone"}
+          value={formData.phone}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+};
+
+export default FormInputs;
--- src/utils/constant/index.ts
+++ src/utils/constant/index.ts
@@ -2,6 +2,7 @@ import HomePage from "../../pages/home-page";
 import AboutPage from "../../pages/about-page";
 import Todo from "../../pages/todo";
 import InputFields from "../../pages/input-fields";
+import FormInputs from "../../pages/form-inputs";
 export const list = [
   {
     path: "/",
@@ -18,5 +19,9 @@ export const list = [
   {
     path: "/input",
     element: InputFields,
+  },
+  {
+    path: "/form-inputs",
+    element: FormInputs,
   },
 ];
